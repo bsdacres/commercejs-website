2ad407bc67a5cbbfc9014cd2a88b63692672586c
bsdacres <bsdacres@gmail.com>
2023-05-31 12:27:33 -0400

    Fixed Json error

--- src/components/CollectionsPreview.jsx
+++ src/components/CollectionsPreview.jsx
@@ -6,10 +6,7 @@ export default function CollectionsPreview(props){
     <div class={styles.collections} style={{'background-image': `linear-gradient(to top, rgba(0, 0, 0, .5), rgba(0, 0, 0, 0)), url(${props.background})`}}>
         <h1>
           {props.title}
-<<<<<<< HEAD
           <a>Expore the Collection</a>
-=======
->>>>>>> 4e15d29344ed1b3b45edb73481549dc785737a19
         </h1>
     </div>
   )
--- src/routes/Store/ProductPage.jsx
+++ src/routes/Store/ProductPage.jsx
@@ -3,16 +3,12 @@ import styles from "./styles.module.css"
 import commerce from "~/lib/commerce"
 import { Motion } from "@motionone/solid"
 import { useCartContext } from "~/context/CartContext"
-<<<<<<< HEAD
 import { createStore } from "solid-js/store"
-=======
->>>>>>> 4e15d29344ed1b3b45edb73481549dc785737a19
 
 
 
 export default function ProductPage(props){
   const [quantity, setQuantity] = createSignal(1)
-<<<<<<< HEAD
   const [selected, setSelected] = createStore(null)
   const { cart, setCart } = useCartContext()
   
@@ -20,13 +16,6 @@ export default function ProductPage(props){
     commerce.cart.add(id, amount, variant).then(response => setCart(response)).catch(
       console.log('there was an error')
     )
-=======
-  const [selected, setSelected] = createSignal(null)
-  const { cart, setCart } = useCartContext()
-  
-  function addtoCart(){
-    commerce.cart.add('prod_p6dP5gMjB9wn7k', 1, { 'vgrp_LvJjoPW4nwe0nO' : 'optn_QG375vjZzR5rMO' }).then(response => setCart(response))
->>>>>>> 4e15d29344ed1b3b45edb73481549dc785737a19
     console.log(cart)
   }
 
@@ -55,19 +44,11 @@ export default function ProductPage(props){
             </p>
           </div>
           <div class={styles.variants}>
-<<<<<<< HEAD
               <For each={props.variants} fallback={<div></div>}>
                   {(variant) => <option onclick={() => setSelected(variant.id)} >{variant.name}</option>}
               </For>
           </div>
           <button onclick={() => addtoCart(props.id, quantity,   )} class={styles.button}>
-=======
-          <For each={props.variants} fallback={<div></div>}>
-              {(variant) => <a onclick={() => setSelected(variant.id)} >{variant.name}</a>}
-          </For>
-          </div>
-          <button onclick={addtoCart()} class={styles.button}>
->>>>>>> 4e15d29344ed1b3b45edb73481549dc785737a19
             <p>Add to Shopping Bag</p>
           </button>
         </Motion.div>
